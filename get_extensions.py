1ba67dad001926a2d7b145f0878b977273c22236
Jas Palermo <jasmes@gmail.com>
2022-10-16 01:24:38 -0400

    oof

--- get_extensions.py
+++ get_extensions.py
@@ -11,5 +11,29 @@ def get_extensions(folder):
         all_extensions.append(file.split(".")[-1])
     return list(set(all_extensions))
 
+def get_extensionsv2(folder):
+    os.chdir(folder)
+    files = os.listdir(folder)
+    all_extensions = []
+    for file in files:
+        all_extensions.append(os.path.splitext(file))
+    return list(set(all_extensions))
 
-print(get_extensions(pic_dir))
+sex = get_extensionsv2(pic_dir)
+n = 0
+for x in sex:
+    print(f"{x}\n")
+i = 0
+
+for i in range(len(sex)):
+    print(sex[(i)])
+
+
+
+sexy = str(sex)
+remove = "[(',)]"
+splitsexy = sexy.strip(remove)
+
+print(splitsexy)
+print(sex)
+print("\n", sexy)
